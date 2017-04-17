dc1d59487b929d8ca74edefd2fdddf7f2fd2f985
allenYetu211 <allenwill211@gmail.com>
2017-04-18 01:07:48 +0800

    TEST.

---
+++ src/View/index/index.vue
@@ -0,0 +1,19 @@
+<template>
+ <div>
+  <span>info
+    <b>22</b>
+  </span>
+ </div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+span {
+  font-size: 20px;
+  b {
+    font-size: 50px;
+  }
+}
+</style>
